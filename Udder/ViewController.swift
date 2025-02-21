a65c264606cabbbdd1ab02a0ea29aeeda2f60367
Hoa Klein <hoaboat@gmail.com>
2025-02-20 20:28:56 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Hoa Doan on 2/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
